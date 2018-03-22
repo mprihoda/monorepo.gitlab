24267a9fcc2c485c96930fc41954bb2e81458994
Marcos Cano <>
2018-03-22 11:59:12 -0600

    @mcano - creates /.SKIP_THIS_JOB file instead

--- build_if_changed.sh
+++ build_if_changed.sh
@@ -12,17 +12,17 @@ command=${@:2}
 DIRNAME="$(dirname $(readlink -f "$0"))"
 ${DIRNAME}/changes.sh ${folder}  || {
   pprint "Warn" "Skipping build for '${folder}'."
+  echo "1" > ${ROOT}/.SKIP_THIS_JOB
   exit 0
 } && {
   pprint "other" "Building '${folder}'..."
   pprint "other" "Executing '${command}'..."
-
-  ${command} && {
-    pprint "$0 Successfull"
-  } || {
-    pprint "$0 Failed"
-    exit 1
-  }
+  # ${command} && {
+  #   pprint "$0 Successfull"
+  # } || {
+  #   pprint "$0 Failed"
+  #   exit 1
+  # }
 
   exit 0
 
