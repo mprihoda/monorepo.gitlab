1d4c136499d4f06b77a360c608ee80c2f0f59a48
Marcos Cano <>
2018-03-21 18:17:02 -0600

    @mcano - pprint

--- changes.sh
+++ changes.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
+set -e
+# reusing common_functions
+cwd="$(dirname "$0")"
+source $cwd/common_functions.sh || {
+    source $(find -name common_functions.sh)
+}
+
 folder=${1}
 DIRNAME="$(dirname $(readlink -f "$0"))"
 ref=$(cat "${DIRNAME}/.LAST_GREEN_COMMIT") 
 # Always indicate changes unless valid green commit ref given, #1
 if [[ ! ${ref:+1} ]]; then 
-  echo 'No LAST_GREEN_COMMIT. Assuming changes.'
+  pprint "other" 'No LAST_GREEN_COMMIT. Assuming changes.'
   exit 0
 fi
 
@@ -13,8 +20,8 @@ echo "Checking for changes of folder '${folder}' from ref '${ref}'..."
 git diff ${ref} --name-only | grep -qw "^${folder}"
 changes=$?
 if [[ ${changes} -eq 0 ]]; then
-  echo "Folder '${folder}' has changed."
+  pprint "other" "Folder '${folder}' has changed."
 else
-  echo "Folder '${folder}' has not changed."
+  pprint "error" "Folder '${folder}' has not changed."
 fi
-exit ${changes}
+exit 0
