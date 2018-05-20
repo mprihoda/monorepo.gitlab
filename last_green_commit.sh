66a7846ef9eddeee8e1af6fc328793c4835b4be8
Daniel Kreuer <dkreuer@users.noreply.github.com>
2018-05-20 19:58:50 +0200

    Extend script to autodetect server host
    Before this commit one had to manually set the Gitlab CI_SERVER_URL in the CI variables. The code change uses the Gitlab CI variables to determine the Gitlab API endpoint automatically.

--- last_green_commit.sh
+++ last_green_commit.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 set -e
-: ${CI_SERVER_URL:="https://gitlab.com"}
+: ${CI_SERVER_URL:=$CI_PROJECT_URL}
 
+CI_SERVER_URL=${CI_SERVER_URL%$CI_PROJECT_PATH}
+CI_SERVER_URL=${CI_SERVER_URL%/}
 
 DIRNAME="$(dirname $(readlink -f "$0"))"
 pushd ${DIRNAME}
 url="${CI_SERVER_URL}/api/v4/projects/${CI_PROJECT_ID}/pipelines?private_token=${PRIVATE_TOKEN}&status=success&ref=${CI_COMMIT_REF_NAME}"
+
 if [ $(which curl 2>/dev/null;) ] && [ $(which jq 2>/dev/null;) ]; then
   commit=$(curl -s ${url} | jq -r -f jq.filter)
 else
